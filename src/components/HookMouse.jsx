57a371e58b622f8355b70957c24fdb5fd6f78a11
Neha <nehaprajapati1150@gmail.com>
2022-01-14 12:46:44 +0530

    useEffect cleanup

--- src/components/HookMouse.jsx
+++ src/components/HookMouse.jsx
@@ -5,12 +5,21 @@ const [x,setX]=useState(0)
 const [y,setY]=useState(0)
 
 const logMousePosition=e=>{
+    console.log('mouse event');
     setX(e.clientX)
     setY(e.clientY)
 }
 
 useEffect(()=>{
+    console.log('useEffcet called');
     window.addEventListener('mousemove',logMousePosition)
+
+
+    //cleanup code
+    return ()=>{
+        console.log('component unmounting');
+        window.removeEventListener('mousemove',logMousePosition)
+    }
 },[])   //empty array denotes that this effect dont depend on any props or state means render only once or useeffect calls only at initial not every render
         //componentDidMount by adding empty array
     return (
