8ceb1d32bb5de839bfa58099d49e194bbb048af0
Neha <nehaprajapati1150@gmail.com>
2022-01-14 13:14:17 +0530

    useEffect with dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import HookMouse from './components/HookMouse';
 import MouseContainer from './components/MouseContainer';
+import IntervalCounter from './components/IntervalCounter';
 
 function App() {
   return (
     <div className="App">
 {/* <HookMouse/> */}
-<MouseContainer/>
+{/* <MouseContainer/> */}
+<IntervalCounter/>
     </div>
   );
 }
---
+++ src/components/IntervalCounter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useEffect, useState } from 'react/cjs/react.development'
+
+function IntervalCounter() {
+const [count,setCount]=useState(0)
+
+const tick =()=>{
+    setCount(prevCount=>prevCount+1)
+}
+
+useEffect(()=>{
+    const interval = setInterval(tick,1000)
+    return ()=>{
+        clearInterval(interval)
+    }
+},[])
+    return (
+        <div>
+            {count}
+        </div>
+    )
+}
+
+export default IntervalCounter
