852f7df1af1c674dcad3d375eabd2fbfe58617ae
Neha <nehaprajapati1150@gmail.com>
2022-01-14 21:54:22 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import React from 'react';
 // import IntervalCounter from './components/IntervalCounter';
 // import DataFetching from './components/DataFetching';
 import ComponentA from './components/ComponentA';
+import CounterOne from './components/CounterOne';
 
 export const UserContext=React.createContext()
 export const ChannelContext=React.createContext()
 
 function App() {
   return (
-    <div className="App">
-<UserContext.Provider value={'neha'}>
-  <ChannelContext.Provider value={'mysterious maya'}>
-  <ComponentA/>
-  </ChannelContext.Provider>
-</UserContext.Provider>
-    </div>
+//     <div className="App">
+// <UserContext.Provider value={'neha'}>
+//   <ChannelContext.Provider value={'mysterious maya'}>
+//   <ComponentA/>
+//   </ChannelContext.Provider>
+// </UserContext.Provider>
+//     </div>
+
+<div>
+  <CounterOne/>
+</div>
   )
 }
 
